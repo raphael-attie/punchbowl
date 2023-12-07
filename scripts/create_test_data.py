8d60c95ebd4a2ff95ca21783569c76b9ba1f29be
Chris Lowder <chris.lowder@swri.org>
2023-12-07 14:11:36 -0700

    creates script function to generate FITS output for header validation

--- scripts/create_test_data.py
+++ scripts/create_test_data.py
@@ -1,9 +1,11 @@
 """Creates test data with the appropriate metadata for punchbowl"""
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import numpy as np
 from astropy.wcs import WCS
+
+from astropy.nddata import StdDevUncertainty
 
 from punchbowl.data import PUNCHData, NormalizedMetadata
 
@@ -29,6 +31,24 @@ def create_punchdata_test_data(path="../punchbowl/tests/"):
     obj.write(file_path, overwrite=True)
 
 
+def create_header_validation_test_data(path="../punchbowl/tests/"):
+    m = NormalizedMetadata.load_template("PTM", "3")
+    m['DATE-OBS'] = datetime.utcnow().isoformat()
+    m['DATE-BEG'] = datetime.utcnow().isoformat()
+    m['DATE-AVG'] = (datetime.utcnow() + timedelta(minutes=2)).isoformat()
+    m['DATE-END'] = (datetime.utcnow() + timedelta(minutes=4)).isoformat()
+    m['DATE'] = (datetime.utcnow() + timedelta(hours=1)).isoformat()
+    h = m.to_fits_header()
+
+    data = np.ones((2, 4096, 4096), dtype=np.float32)
+    uncertainty = StdDevUncertainty(np.sqrt(np.abs(data)).astype(np.int8))
+
+    d = PUNCHData(data=data, uncertainty=uncertainty, wcs=WCS(h), meta=m)
+    file_path = os.path.join(path, "test_header_validation.fits")
+    d.write(file_path, overwrite=True)
+
+
 if __name__ == "__main__":
+    create_header_validation_test_data()
     create_f_corona_test_data()
     create_punchdata_test_data()
