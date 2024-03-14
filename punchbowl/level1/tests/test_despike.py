56c671187cdbccb8bbd4020de92c2c68e14477af
Marcus Hughes <hughes.jmb@gmail.com>
2024-03-14 07:42:41 -0600

    expands spikejones test

--- punchbowl/level1/tests/test_despike.py
+++ punchbowl/level1/tests/test_despike.py
@@ -29,3 +29,6 @@ def test_spikejones_with_one_spike():
     output = spikejones(image)
 
     assert output.shape == image.shape
+    assert output[50, 50]
+    assert output[49, 50]
+    assert output[30, 70]
