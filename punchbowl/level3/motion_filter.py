61014ce566d5dac7be2458ba3b9523671ae2a6fb
s0larish <risht42@gmail.com>
2024-05-03 15:04:05 -0600

    Updated documentation in motion_filer.py

--- punchbowl/level3/motion_filter.py
+++ punchbowl/level3/motion_filter.py
@@ -1,7 +1,4 @@
 import numpy as np
-import scipy.fft
-import warnings
-from scipy import signal
 from skimage.filters import window
 from scipy.fftpack import fftn, ifftn, fftshift, ifftshift
 from sunpy import log
@@ -14,30 +11,76 @@ except ImportError:
     HAS_CUPY = False
 
 
-# Defining cicular mask
 def layer_mask(radius: float, img_shape) -> np.ndarray:
+    """Defines a circular mask
+
+    Parameters
+    ----------
+    radius : float
+        The radius of the circular mask.
+    img_shape : tuple
+        The shape of the input image.
+
+    Returns
+    -------
+    np.ndarray
+        Circular mask.
+    """
     xs = np.arange(0, img_shape[0])
     ys = np.arange(0, img_shape[1])
     xs, ys = np.meshgrid(xs, ys)
-    distance = np.sqrt((xs-img_shape[0]/2)**2 + (ys-img_shape[1]/2)**2)
+    distance = np.sqrt((xs - img_shape[0] / 2) ** 2 + (ys - img_shape[1] / 2) ** 2)
     return distance < radius
 
-# Creating hourglass filter
+
 def gen_filter(fftcube, cutoff_vel):
+    """Creates an hourglass filter mask
 
+    Parameters
+    ----------
+    fftcube : np.ndarray
+        The 3D Fourier space cube.
+    cutoff_vel : float
+        The cutoff velocity.
+
+    Returns
+    -------
+    np.ndarray
+        Hourglass filter mask.
+    """
     fft_shape = fftcube.shape
-    img_shape = (fft_shape[1],fft_shape[2])
-    cutoff = cutoff_vel #(*some factor to be estimated next)
+    img_shape = (fft_shape[1], fft_shape[2])
+    cutoff = cutoff_vel  # (*some factor to be estimated next)
 
-    mask1 = np.stack([layer_mask(radius, img_shape) for radius in np.linspace(int(fft_shape[1]/2), cutoff, int(fft_shape[0]/2))], axis=0)
-    mask2 = np.stack([layer_mask(radius, img_shape) for radius in np.linspace( cutoff, int(fft_shape[1]/2), int(fft_shape[0]/2))], axis=0)
+    mask1 = np.stack(
+        [layer_mask(radius, img_shape) for radius in np.linspace(int(fft_shape[1] / 2), cutoff, int(fft_shape[0] / 2))],
+        axis=0)
+    mask2 = np.stack(
+        [layer_mask(radius, img_shape) for radius in np.linspace(cutoff, int(fft_shape[1] / 2), int(fft_shape[0] / 2))],
+        axis=0)
 
-    filt_mask = np.concatenate((mask1,mask2),axis=0)
+    filt_mask = np.concatenate((mask1, mask2), axis=0)
     return filt_mask
 
 
-# Generating motion filtered data
-def motion_filter(stacked_data, apod_margin, use_gpu=True):
+def motion_filter(stacked_data:np.ndarray,
+                  apod_margin,
+                  use_gpu=True):
+    """Performs a Fourier motion filter on input datacube
+    Parameters
+    ----------
+    stacked_data : np.ndarray
+        Starfield removed stacked datacube.
+    apod_margin : int
+        Apodization margin.
+    use_gpu : bool, optional
+        Whether to use GPU for processing (default is True).
+
+    Returns
+    -------
+    np.ndarray
+        Fourier motion filtered datacube.
+    """
     padded_data = np.pad(stacked_data, ((apod_margin * 2, apod_margin * 2), (apod_margin, apod_margin),
                                         (apod_margin, apod_margin)), mode='constant', constant_values=0)
 
