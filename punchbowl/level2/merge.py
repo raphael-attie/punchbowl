9fbfb98a65d9f8102e1e6098cda3ade116aa1fe9
Marcus Hughes <hughes.jmb@gmail.com>
2024-07-29 12:25:45 -0600

    improve docstring

--- punchbowl/level2/merge.py
+++ punchbowl/level2/merge.py
@@ -8,20 +8,17 @@ from prefect import task
 
 @task
 def merge_many_task(data: list[NDCube], trefoil_wcs: WCS) -> NDCube:
-    """Merge many task."""
+    """Merge many task and carefully combine uncertainties."""
     reprojected_data = np.stack([d.data for d in data], axis=-1)
     reprojected_uncertainty = np.stack([d.uncertainty.array for d in data], axis=-1)
 
-    # Merge these data
     w = np.nansum(1/np.square(reprojected_data), axis=-1)
     trefoil_data = np.nansum(reprojected_data / np.square(reprojected_uncertainty), axis=2) / w
     trefoil_uncertainty = np.sqrt(1 / w)
 
-    # Pack up an output data object
-    data_object = NDCube(
-        trefoil_data, uncertainty=StdDevUncertainty(trefoil_uncertainty), wcs=trefoil_wcs, meta=data[0].meta,
+    return NDCube(
+        trefoil_data,
+        uncertainty=StdDevUncertainty(trefoil_uncertainty),
+        wcs=trefoil_wcs,
+        meta=data[0].meta,  # TODO: how does the meta get merged?
     )
-    data_object.meta["LEVEL"] = "2"
-    # TODO: what do we do with the meta data???? shouldn't it merge
-
-    return data_object
