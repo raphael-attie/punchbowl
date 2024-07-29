64c7584c83eae256dd26fd4b859f6db6933f5283
Marcus Hughes <hughes.jmb@gmail.com>
2024-07-29 10:07:19 -0600

    make trefoil function accessible

--- punchbowl/data/__init__.py
+++ punchbowl/data/__init__.py
@@ -1,5 +1,11 @@
 from punchbowl.data.history import History
 from punchbowl.data.io import get_base_file_name, load_ndcube_from_fits, write_ndcube_to_fits
 from punchbowl.data.meta import NormalizedMetadata
+from punchbowl.data.wcs import load_trefoil_wcs
 
-__all__ = ["History", "NormalizedMetadata", "load_ndcube_from_fits", "write_ndcube_to_fits", "get_base_file_name"]
+__all__ = ["History",
+           "NormalizedMetadata",
+           "load_ndcube_from_fits",
+           "write_ndcube_to_fits",
+           "get_base_file_name",
+           "load_trefoil_wcs"]
