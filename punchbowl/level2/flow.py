6994f94ecc4ca6e47e7ebb5119e1bcf5999e8a8b
Chris Lowder <chris.lowder@swri.org>
2024-09-13 15:14:30 -0600

    adds quickPUNCH flow and notes

--- punchbowl/level2/flow.py
+++ punchbowl/level2/flow.py
@@ -3,6 +3,7 @@ from ndcube import NDCube
 from prefect import flow, get_run_logger
 
 from punchbowl.data import get_base_file_name, load_trefoil_wcs
+from punchbowl.data.wcs import load_quickpunch_mosaic_wcs, load_quickpunch_nfi_wcs
 from punchbowl.exceptions import IncorrectFileCountError
 from punchbowl.level2.merge import merge_many_task
 from punchbowl.level2.polarization import resolve_polarization_task
@@ -60,13 +61,62 @@ def level2_core_flow(data_list: list[str] | list[NDCube],
     logger.info("ending level 2 core flow")
     return [output_data]
 
+# TODO - Split this into multiple flows?
+# TODO - Generate quickPUNCH f-corona products?
+@flow(validate_parameters=False)
+def levelq_core_flow(data_list: list[str] | list[NDCube],
+                     output_filename: str | None = None) -> list[NDCube]:
+    """Level quickPUNCH core flow."""
+    logger = get_run_logger()
+    logger.info("beginning level quickPUNCH core flow")
+    # TODO - Check file input number for quickpunch products
+    if len(data_list) != 12:
+        msg = f"Received {len(data_list)} files when need 12."
+        raise IncorrectFileCountError(msg)
+
+    data_list = [load_image_task(d) if isinstance(d, str) else d for d in data_list]
+
+    # TODO - Check whether a separate quickPUNCH order should be defined at the top - no polarization
+    # order the data list so it can be processed properly
+    ordered_data_list = []
+    for order_element in ORDER:
+        found = False
+        for data_element in data_list:
+            typecode = data_element.meta["TYPECODE"].value
+            obscode = data_element.meta["OBSCODE"].value
+            if typecode == order_element[:2] and obscode == order_element[2]:
+                ordered_data_list.append(data_element)
+                found = True
+        if not found:
+            msg = f"Did not receive {order_element} file."
+            raise IncorrectFileCountError(msg)
+    logger.info(f"Ordered files are {[get_base_file_name(cube) for cube in ordered_data_list]}")
+
+    quickpunch_mosaic_wcs, quickpunch_mosaic_shape = load_quickpunch_mosaic_wcs()
+    quickpunch_nfi_wcs, quickpunch_nfi_shape = load_quickpunch_nfi_wcs()
+
+    data_list_mosaic = reproject_many_flow(data_list, quickpunch_mosaic_wcs, quickpunch_mosaic_shape)
+    data_list_nfi = reproject_many_flow(data_list, quickpunch_nfi_wcs, quickpunch_nfi_shape)
+
+    # TODO - Skip merge for NFI?
+    output_data_mosaic = merge_many_task(data_list_mosaic, quickpunch_mosaic_wcs)
+    output_data_nfi = merge_many_task(data_list_nfi, quickpunch_nfi_wcs)
+
+    # TODO - Separate filenames needed / split into two flows?
+    if output_filename is not None:
+        output_image_task(output_data_mosaic, output_filename)
+        output_image_task(output_data_nfi, output_filename)
+
+    logger.info("ending level quickPUNCH core flow")
+    return [output_data_mosaic, output_data_nfi]
+
 
 if __name__ == "__main__":
     import os
     import glob
 
-    filenames = sorted(glob.glob("/Users/jhughes/Desktop/data/gamera_mosaic_jan2024/forward_l1/*.fits"),
+    filenames = sorted(glob.glob("/Users/clowder/data/punch/gamera_mosaic_jan2024/forward_l1/*.fits"),
                        key=lambda s: os.path.basename(s).split("_")[3])
 
-    level2_core_flow(filenames,
-                     output_filename="/Users/jhughes/Desktop/data/gamera_mosaic_jan2024/forward_l2/test_l2.fits")
+    levelq_core_flow(filenames,
+                     output_filename="/Users/clowder/data/punch/gamera_mosaic_jan2024/forward_l2/test_l2.fits")
