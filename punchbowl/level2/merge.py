5bdf4b8774411e5e8d41c34da201940f86a4d013
Marcus Hughes <hughes.jmb@gmail.com>
2024-09-24 08:55:31 -0600

    repair tests

--- punchbowl/level2/merge.py
+++ punchbowl/level2/merge.py
@@ -13,15 +13,20 @@ def merge_many_task(data: list[NDCube], trefoil_wcs: WCS) -> NDCube:
     """Merge many task and carefully combine uncertainties."""
     trefoil_data_layers, trefoil_uncertainty_layers = [], []
     for polarization in [-60, 0, 60]:
-        reprojected_data = np.stack([d.data for d in data if d.meta["POLAR"].value == polarization], axis=-1)
-        reprojected_weights = np.stack([1/np.square(d.uncertainty.array)
-                                        for d in data if d.meta["POLAR"].value == polarization],
-                                           axis=-1)
-        reprojected_weights[reprojected_weights <= 0] = 1E-8
+        selected_images = [d.data for d in data if d.meta["POLAR"].value == polarization]
+        if len(selected_images) > 0:
+            reprojected_data = np.stack(selected_images, axis=-1)
+            reprojected_weights = np.stack([1/np.square(d.uncertainty.array)
+                                            for d in data if d.meta["POLAR"].value == polarization],
+                                               axis=-1)
+            reprojected_weights[reprojected_weights <= 0] = 1E-8
 
-        trefoil_data_layers.append(np.nansum(reprojected_data * reprojected_weights, axis=2) /
-                                   np.nansum(reprojected_weights, axis=2))
-        trefoil_uncertainty_layers.append(1/np.nansum(np.sqrt(reprojected_weights), axis=2))
+            trefoil_data_layers.append(np.nansum(reprojected_data * reprojected_weights, axis=2) /
+                                       np.nansum(reprojected_weights, axis=2))
+            trefoil_uncertainty_layers.append(1/np.nansum(np.sqrt(reprojected_weights), axis=2))
+        else:
+            trefoil_data_layers.append(np.zeros((4096, 4096)))
+            trefoil_uncertainty_layers.append(np.zeros((4096, 4096))-999)
 
     output_meta = NormalizedMetadata.load_template("PTM", "2")
     output_meta["DATE-OBS"] = data[0].meta["DATE-OBS"].value  # TODO: do this better and fill rest of meta
