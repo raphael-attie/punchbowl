9a045f5a448cdd5f812dd8823dc56196ff7b0af3
Marcus Hughes <hughes.jmb@gmail.com>
2024-11-01 19:27:26 -0600

    add new f corona model generation algorithm

--- punchbowl/level3/f_corona_model.py
+++ punchbowl/level3/f_corona_model.py
@@ -1,140 +1,80 @@
-import warnings
-from datetime import datetime
-
 import numpy as np
 from ndcube import NDCube
+from numpy.polynomial import polynomial
 from prefect import get_run_logger, task
+from quadprog import solve_qp
 
 from punchbowl.data import load_ndcube_from_fits
 from punchbowl.exceptions import InvalidDataError
 
 
-@task
-def query_f_corona_model_source(
-    polarizer: str, product: str, start_datetime: datetime, end_datetime: datetime,
-) -> list[str]:
+def solve_qp_cube(input_vals: np.ndarray, cube: np.ndarray) -> np.ndarray:
     """
-    Create a list of files for later F corona model generation.
-
-    Creates a list of files based between a start date/time (start_datetime)
-    and an end date/time (end_datetime) for a specified polarizer and
-    PUNCH_product. The start and end times can both be input explicitly,
-    individually, or derived from a mid time.
-
-    if start_datetime (datetime object) and an end_datetime(datetime object)
-    are specified a list of files between those dates is produced for the
-    specified polarizer and PUNCH_product.
-
+    Fast solver for the quadratic programming problem.
 
     Parameters
     ----------
-    polarizer : string [= 'clear', '-60', '60', '0' ]
-        input a string specifying the type of polarizer to search for
-
-    product : string [= 'mosaic', 'nfi']
-        product code to analyze
-
-    start_datetime : datetime
-        input a start_datetime of interest.
-
-    end_datetime : datetime
-        input a start_datetime of interest.
+    input_vals : np.ndarray
+        array of times
+    cube : np.ndarray
+        array of data
 
     Returns
     -------
-    file_list : [list]
-        returns a list of files over the specified period for the specified
-        polarizer.
+    np.ndarray
+        Array of coefficients for solving polynomial
 
     """
-    logger = get_run_logger()
-    logger.info("query_f_corona_model_source started")
+    c = np.transpose(input_vals)
+    g = np.matmul(c, input_vals)
 
-    # Check for polarizer input, and if valid
-    if polarizer not in ["clear", "-60", "60", "0"]:
-        msg = f"input polarizer expected as string: 'clear', '-60', '60', '0'. Found {polarizer}."
-        raise ValueError(msg)
+    sol = np.zeros((input_vals.shape[1], cube.shape[1], cube.shape[2]))
 
-    # Check for PUNCH input, and if valid
-    if product not in ["nfi", "mosaic"]:
-        msg = f"input PUNCH_product expected as string:  'nfi', 'mosaic'. Found {product}"
-        raise ValueError(msg)
+    for i in range(cube.shape[1]):
+        for j in range(cube.shape[2]):
+            a = np.matmul(c, cube[:, i, j])
+            try:
+                this_sol = solve_qp(g, a, c, cube[:, i, j])
+            except ValueError:
+                this_sol = np.zeros(input_vals.shape[1])
+            for k in range(sol.shape[0]):
+                sol[k, i, j] = this_sol[k]
 
-    file_list = []
-    # file_list=session.query(File).where(and_(File.state == "created",
-    #                                           File.date_obs >= date.today() + relativedelta(months=-1))).all()
+    return np.asarray(sol)
 
-    # Check if files found in range
-    if len(file_list) == 0:
-        warnings.warn(f"No files found over specified dates: from: {start_datetime} to: {end_datetime}.", stacklevel=2)
-
-    logger.info("query_f_corona_model_source finished")
-
-    return file_list
-
-
-def _zvalue_from_index(arr, ind):  # noqa: ANN202, ANN001
+def model_fcorona_for_cube(xt: np.ndarray,
+                          cube: np.ndarray,
+                          smooth_level: float | None =4) -> np.ndarray:
     """
-    Do math.
+    Model the F corona given a list of times and a corresponding data cube, .
 
-    Private helper function to work around the limitation of np.choose() by employing np.take().
-    arr has to be a 3D array
-    ind has to be a 2D array containing values for z-indicies to take from arr
-    See: http://stackoverflow.com/a/32091712/4169585
-    This is faster and more memory efficient than using the ogrid based solution with fancy indexing.
+    Parameters
+    ----------
+    xt : np.ndarray
+        time array
+    cube : np.ndarray
+        observation array
+    smooth_level : float | None
+        If None, no smoothing is applied.
+        Otherwise, the top and bottom `smooth_level` standard deviations of data are rejected.
+
+    Returns
+    -------
+    np.ndarray
+
     """
-    # get number of columns and rows
-    _, n_cols, n_rows = arr.shape
+    average = np.mean(cube, axis=0)
+    std = np.nanstd(cube, axis=0)
+    if smooth_level is not None:
+        a, b, c = np.where(cube[:, ...] > (average - (smooth_level * std)))
+        cube[a, b, c] = average[b, c]
 
-    # get linear indices and extract elements with np.take()
-    idx = n_cols*n_rows*ind + n_rows*np.arange(n_rows)[:,None] + np.arange(n_cols)
-    return np.take(arr, idx)
+        a, b, c = np.where(cube[:, ...] < (average - (smooth_level * std)))
+        cube[a, b, c] = average[b, c]
 
-
-def nan_percentile(arr: np.ndarray, q: list[float] | float) -> np.ndarray:
-    """Calculate the nan percentile faster of a 3D cube."""
-    # np.nanpercentile is slow so use this: https://krstn.eu/np.nanpercentile()-there-has-to-be-a-faster-way/
-
-    # valid (non NaN) observations along the first axis
-    valid_obs = np.sum(np.isfinite(arr), axis=0)
-    # replace NaN with maximum
-    max_val = np.nanmax(arr)
-    arr[np.isnan(arr)] = max_val
-    # sort - former NaNs will move to the end
-    arr = np.sort(arr, axis=0)
-
-    # loop over requested quantiles
-    if isinstance(q, list):
-        qs = []
-        qs.extend(q)
-    else:
-        qs = [q]
-    if len(qs) < 2:
-        quant_arr = np.zeros(shape=(arr.shape[1], arr.shape[2]))
-    else:
-        quant_arr = np.zeros(shape=(len(qs), arr.shape[1], arr.shape[2]))
-
-    result = []
-    for i in range(len(qs)):
-        quant = qs[i]
-        # desired position as well as floor and ceiling of it
-        k_arr = (valid_obs - 1) * (quant / 100.0)
-        f_arr = np.floor(k_arr).astype(np.int32)
-        c_arr = np.ceil(k_arr).astype(np.int32)
-        fc_equal_k_mask = f_arr == c_arr
-
-        # linear interpolation (like numpy percentile) takes the fractional part of desired position
-        floor_val = _zvalue_from_index(arr=arr, ind=f_arr) * (c_arr - k_arr)
-        ceil_val = _zvalue_from_index(arr=arr, ind=c_arr) * (k_arr - f_arr)
-
-        quant_arr = floor_val + ceil_val
-        # if floor == ceiling take floor value
-        quant_arr[fc_equal_k_mask] = _zvalue_from_index(arr=arr, ind=k_arr.astype(np.int32))[fc_equal_k_mask]
-
-        result.append(quant_arr)
-
-    return result
-
+    input_array = np.c_[np.power(xt, 3), np.square(xt), xt, np.ones(len(xt))]
+    out = -solve_qp_cube(input_array, -cube)
+    return polynomial.polyval(xt[len(xt)//2], out[::-1, :, :])
 
 @task
 def construct_f_corona_background(
@@ -145,8 +85,6 @@ def construct_f_corona_background(
     logger = get_run_logger()
     logger.info("construct_f_corona_background started")
 
-    # create an empty array to fill with data
-    #   open the first file in the list to ge the shape of the file
     if len(data_list) == 0:
         msg = "data_list cannot be empty"
         raise ValueError(msg)
@@ -162,16 +100,16 @@ def construct_f_corona_background(
     data_cube = np.empty((number_of_data_frames, *data_shape), dtype=float)
     uncertainty_cube = np.empty((number_of_data_frames, *data_shape), dtype=float)
     meta_list = []
+    obs_times = []
 
     for i, address_out in enumerate(data_list):
         data_object = load_ndcube_from_fits(address_out)
         data_cube[i, :, :] = data_object.data[layer]
         uncertainty_cube[i, :, :] = data_object.uncertainty.array[layer]
+        obs_times.append(data_object.meta.datetime.timestamp())
         meta_list.append(data_object.meta)
 
-
-    # create an output PUNCHdata object
-    f_background = np.zeros_like(data_cube[0])
+    f_background = model_fcorona_for_cube(obs_times, data_cube)
     output = NDCube(f_background, wcs=output_wcs, meta=output_meta, mask=output_mask)
 
     logger.info("construct_f_corona_background finished")
@@ -219,14 +157,18 @@ def subtract_f_corona_background(data_object: NDCube,
         msg = "After F corona model was before the observation date"
         raise InvalidDataError(msg)
 
-    interpolated_model = ((after_f_background_model.data - before_f_background_model.data)
-                          * (observation_date - before_date) / (after_date - before_date)
-                          + before_f_background_model.data)
+    if before_date == observation_date:
+        interpolated_model = before_f_background_model
+    elif after_date == observation_date:
+        interpolated_model = after_f_background_model
+    else:
+        interpolated_model = ((after_f_background_model.data - before_f_background_model.data)
+                              * (observation_date - before_date) / (after_date - before_date)
+                              + before_f_background_model.data)
+
     interpolated_model[np.isinf(data_object.uncertainty.array)] = 0
 
-    bkg_subtracted_data = data_object.data - interpolated_model
-
-    data_object.data[...] = bkg_subtracted_data
+    data_object.data[...] = data_object.data[...] - interpolated_model
     data_object.uncertainty.array[:, :] -= interpolated_model
     return data_object
 
