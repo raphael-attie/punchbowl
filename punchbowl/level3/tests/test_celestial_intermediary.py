4bdb7d36e4d1eddc8c3d2e9608194b171f30e144
Sam Van Kooten <vankooten.sam@gmail.com>
2024-11-15 18:23:55 -0600

    Add code for doing "reusable" celestial projections, to benefit secondary starfield removal

---
+++ punchbowl/level3/tests/test_celestial_intermediary.py
@@ -0,0 +1,193 @@
+import os
+
+import numpy as np
+import pytest
+import reproject
+from astropy.io import fits
+from astropy.wcs import WCS
+
+from punchbowl.level3 import celestial_intermediary
+
+TESTDATA_DIR = os.path.dirname(__file__)
+TEST_FILE = TESTDATA_DIR + '/data/downsampled_L2_PTM.fits'
+
+
+def test_to_celestial_frame_cutout():
+    image, wcs = celestial_intermediary.to_celestial_frame_cutout(TEST_FILE, cdelt=.2)
+    assert np.any(np.isfinite(image))
+
+    assert 0 <= wcs.wcs.crval[0] < 360
+    assert wcs.wcs.crval[1] == 0
+    assert wcs.wcs.cdelt[0] == wcs.wcs.cdelt[1]
+    assert tuple(wcs.wcs.ctype) == ('RA---CAR', 'DEC--CAR')
+
+
+# A point of concern is whether this shifting of reprojected images works across the RA=0 wrap point, so we test that
+# well here.
+@pytest.mark.parametrize('shift1,shift2,is_overlap',
+                          # This is a "benign" pair, nowhere near the RA=0 point
+                         [((0, 0), (-12, 5), True),
+                          # This pair is right on either side of RA=0, but neither contains that point
+                          ((40, 12), (-150, 0), False),
+                          # The first one is at high RA and the second one straddles RA=0
+                          ((40, 12), (-230, 0), True),
+                          # Now one at low RA and one straddling RA=0
+                          ((-150, 0), (-230, 0), True),
+                         ])
+def test_shift_image_onto(tmp_path, shift1, shift2, is_overlap):
+    # Generate a test file, where we customize which portion of the sky it covers
+    file1 = tmp_path / 'shifted_file1.fits'
+    with fits.open(TEST_FILE) as hdul:
+        del hdul[2]
+        hdul[1].header['CRVAL1A'] += shift1[0]
+        hdul[1].header['CRVAL2A'] += shift1[1]
+        hdul[1].data = hdul[1].data[0]
+        hdul.writeto(file1)
+
+    # Generate another test file, which covers a different portion of the sky
+    file2 = tmp_path / 'shifted_file2.fits'
+    with fits.open(TEST_FILE) as hdul:
+        del hdul[2]
+        hdul[1].header['CRVAL1A'] += shift2[0]
+        hdul[1].header['CRVAL2A'] += shift2[1]
+        hdul[1].data = hdul[1].data[0]
+        hdul.writeto(file2)
+
+    reproj_data_1, wcs1 = celestial_intermediary.to_celestial_frame_cutout(file1, cdelt=.4)
+    reproj_data_2, wcs2 = celestial_intermediary.to_celestial_frame_cutout(file2, cdelt=.4)
+
+    # Just sanity-check that these are different images and different outputs, so we know the rest of the test is valid
+    assert wcs1.wcs.crval[0] != wcs2.wcs.crval[0]
+    assert reproj_data_1.shape != reproj_data_2.shape or not np.testing.assert_allclose(
+        reproj_data_1, reproj_data_2, equal_nan=True)
+
+    # And sanity-check that the data came out valid
+    assert np.any(np.isfinite(reproj_data_1))
+    assert np.any(np.isfinite(reproj_data_2))
+
+    # The celestial intermediary images should be different cutouts of the sky, but on the same pixel grid. We test
+    # that by aligning them first with this function, which simply adds and removes rows of pixels, and then by
+    # actually reprojecting the data into the same output frame.
+    d1_shifted_to_2 = celestial_intermediary.shift_image_onto(reproj_data_1, reproj_data_2, wcs1, wcs2)
+    assert not is_overlap or np.any(np.isfinite(d1_shifted_to_2))
+
+    with fits.open(file1) as hdul:
+        data = hdul[1].data
+        wcs = WCS(hdul[1].header, key='A').celestial
+        d1_reprojected_to_2 = reproject.reproject_adaptive((data, wcs), wcs2, reproj_data_2.shape,
+                                                           return_footprint=False, roundtrip_coords=False,
+                                                           boundary_mode='strict', conserve_flux=True,
+                                                           center_jacobian=True)
+
+    # We should be getting the same thing, to a part in 10^8
+    np.testing.assert_allclose(d1_shifted_to_2, d1_reprojected_to_2, equal_nan=True, rtol=1e-8)
+
+    # Now do it the other way
+    d2_shifted_to_1 = celestial_intermediary.shift_image_onto(reproj_data_2, reproj_data_1, wcs2, wcs1)
+    assert not is_overlap or np.any(np.isfinite(d2_shifted_to_1))
+
+    with fits.open(file2) as hdul:
+        data = hdul[1].data
+        wcs = WCS(hdul[1].header, key='A').celestial
+        d2_reprojected_to_1 = reproject.reproject_adaptive((data, wcs), wcs1, reproj_data_1.shape,
+                                                           return_footprint=False, roundtrip_coords=False,
+                                                           boundary_mode='strict', conserve_flux=True,
+                                                           center_jacobian=True)
+
+    # We should be getting the same thing, to a part in 10^8
+    np.testing.assert_allclose(d2_shifted_to_1, d2_reprojected_to_1, equal_nan=True, rtol=1e-8)
+
+
+def test_shift_image_onto_3d_cube(tmp_path):
+    # Generate a test file, where we customize which portion of the sky it covers
+    file1 = TEST_FILE
+
+    # Generate another test file, which covers a different portion of the sky
+    file2 = tmp_path / 'shifted_file2.fits'
+    with fits.open(TEST_FILE) as hdul:
+        del hdul[2]
+        hdul[1].header['CRVAL1A'] += 10
+        hdul[1].header['CRVAL2A'] += 5
+        hdul.writeto(file2)
+
+    reproj_data_1, wcs1 = celestial_intermediary.to_celestial_frame_cutout(file1, cdelt=.6)
+    reproj_data_2, wcs2 = celestial_intermediary.to_celestial_frame_cutout(file2, cdelt=.6)
+
+    # Just sanity-check that these are different images and different outputs, so we know the rest of the test is valid
+    assert wcs1.wcs.crval[0] != wcs2.wcs.crval[0]
+    assert reproj_data_1.shape != reproj_data_2.shape or not np.testing.assert_allclose(
+        reproj_data_1, reproj_data_2, equal_nan=True)
+
+    # And sanity-check that the data came out valid
+    assert np.any(np.isfinite(reproj_data_1))
+    assert np.any(np.isfinite(reproj_data_2))
+
+    # The celestial intermediary images should be different cutouts of the sky, but on the same pixel grid. We test
+    # that by aligning them first with this function, which simply adds and removes rows of pixels, and then by
+    # actually reprojecting the data into the same output frame.
+    d1_shifted_to_2 = celestial_intermediary.shift_image_onto(reproj_data_1, reproj_data_2, wcs1, wcs2)
+    assert np.any(np.isfinite(d1_shifted_to_2))
+
+    with fits.open(file1) as hdul:
+        data = hdul[1].data
+        wcs = WCS(hdul[1].header, key='A').celestial
+        d1_reprojected_to_2 = reproject.reproject_adaptive((data, wcs), wcs2, reproj_data_2.shape,
+                                                           return_footprint=False, roundtrip_coords=False,
+                                                           boundary_mode='strict', conserve_flux=True,
+                                                           center_jacobian=True)
+
+    # We should be getting the same thing, to a part in 10^8
+    np.testing.assert_allclose(d1_shifted_to_2, d1_reprojected_to_2, equal_nan=True, rtol=1e-8)
+
+    # Now do it the other way
+    d2_shifted_to_1 = celestial_intermediary.shift_image_onto(reproj_data_2, reproj_data_1, wcs2, wcs1)
+    assert np.any(np.isfinite(d2_shifted_to_1))
+
+    with fits.open(file2) as hdul:
+        data = hdul[1].data
+        wcs = WCS(hdul[1].header, key='A').celestial
+        d2_reprojected_to_1 = reproject.reproject_adaptive((data, wcs), wcs1, reproj_data_1.shape,
+                                                           return_footprint=False, roundtrip_coords=False,
+                                                           boundary_mode='strict', conserve_flux=True,
+                                                           center_jacobian=True)
+
+    # We should be getting the same thing, to a part in 10^8
+    np.testing.assert_allclose(d2_shifted_to_1, d2_reprojected_to_1, equal_nan=True, rtol=1e-8)
+
+
+def test_shift_image_onto_fill_value(tmp_path):
+    # Generate a test file, where we customize which portion of the sky it covers
+    file1 = TEST_FILE
+
+    # Generate another test file, which covers a different portion of the sky
+    file2 = tmp_path / 'shifted_file2.fits'
+    with fits.open(TEST_FILE) as hdul:
+        for hdu in hdul[1:]:
+            hdu.header['CRVAL1A'] += 10
+            hdu.header['CRVAL2A'] += 10
+        hdul.writeto(file2)
+
+    reproj_data_1, wcs1 = celestial_intermediary.to_celestial_frame_cutout(file1, cdelt=.6)
+    reproj_data_2, wcs2 = celestial_intermediary.to_celestial_frame_cutout(file2, cdelt=.6)
+
+    d1_shifted_to_2 = celestial_intermediary.shift_image_onto(reproj_data_1, reproj_data_2, wcs1, wcs2)
+    d2_shifted_to_1 = celestial_intermediary.shift_image_onto(reproj_data_2, reproj_data_1, wcs2, wcs1)
+
+    assert np.any(np.isnan(d1_shifted_to_2))
+    assert np.any(np.isnan(d2_shifted_to_1))
+
+    d1_shifted_to_2 = celestial_intermediary.shift_image_onto(
+        np.nan_to_num(reproj_data_1), reproj_data_2, wcs1, wcs2, fill_value=0)
+    d2_shifted_to_1 = celestial_intermediary.shift_image_onto(
+        np.nan_to_num(reproj_data_2), reproj_data_1, wcs2, wcs1, fill_value=0)
+
+    assert not np.any(np.isnan(d1_shifted_to_2))
+    assert not np.any(np.isnan(d2_shifted_to_1))
+
+
+def test_shift_image_onto_different_cdelts():
+    reproj_data_1, wcs1 = celestial_intermediary.to_celestial_frame_cutout(TEST_FILE, cdelt=.8)
+    reproj_data_2, wcs2 = celestial_intermediary.to_celestial_frame_cutout(TEST_FILE, cdelt=1)
+
+    with pytest.raises(ValueError, match=".*WCSes must have identical CDELTs.*"):
+        celestial_intermediary.shift_image_onto(reproj_data_1, reproj_data_2, wcs1, wcs2)
