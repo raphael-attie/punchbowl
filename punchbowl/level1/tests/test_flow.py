b797a494773e5f7e1221e452d5f4ef6439556ec2
Marcus Hughes <hughes.jmb@gmail.com>
2024-11-18 01:49:31 -0700

    improves testing with filename test

--- punchbowl/level1/tests/test_flow.py
+++ punchbowl/level1/tests/test_flow.py
@@ -1,22 +1,27 @@
+import os
 import pathlib
 
 from ndcube import NDCube
 from prefect.testing.utilities import prefect_test_harness
 
+from build.lib.punchbowl.data.io import write_ndcube_to_fits
 from punchbowl.data.tests.test_io import sample_ndcube
 from punchbowl.level1.flow import level1_core_flow
 
-# def test_core_flow_runs_with_filenames(sample_punchdata):
-#     with tempfile.TemporaryDirectory() as tmpdirname:
-#         input_name = tmpdirname + "/test_input.fits"
-#         output_name = tmpdirname + "/test_output.fits"
-#         sample_punchdata(shape=(2048, 2048)).write(input_name)
-#         with prefect_test_harness():
-#             level1_core_flow(input_name, output_filename=output_name)
-#         output = PUNCHData.from_fits(output_name)
-#         assert isinstance(output, PUNCHData)
-
 THIS_DIRECTORY = pathlib.Path(__file__).parent.resolve()
+
+def test_core_flow_runs_with_filenames(sample_ndcube, tmpdir):
+        input_name = os.path.join(tmpdir, "test_input.fits")
+        output_name = os.path.join(tmpdir, "test_output.fits")
+        write_ndcube_to_fits(sample_ndcube(shape=(10, 10), code="CR1", level="0"), input_name)
+        quartic_coefficient_path = THIS_DIRECTORY / "data" / "test_quartic_coeffs.fits"
+        with prefect_test_harness():
+            output = level1_core_flow([input_name],
+                                      quartic_coefficient_path=quartic_coefficient_path,
+                                      output_filename=[output_name])
+        assert isinstance(output[0], NDCube)
+        assert os.path.exists(output_name[0])
+
 
 
 def test_core_flow_runs_with_objects_and_calibration_files(sample_ndcube):
