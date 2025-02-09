a0c5ab27cec7d1610e82818299d1d0f4d269be32
Marcus Hughes <hughes.jmb@gmail.com>
2025-02-08 20:07:11 -0700

    Update __init__.py

--- punchbowl/__init__.py
+++ punchbowl/__init__.py
@@ -1,4 +1,7 @@
 """punchbowl: data reduction and calibration pipeline for PUNCH."""
 import importlib.metadata
 
-__version__ = importlib.metadata.version("punchbowl")
+try:
+  __version__ = importlib.metadata.version("punchbowl")
+except PackageNotFoundError:
+  __version__ = "0.0.0"
