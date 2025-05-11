96c39de227650cfafb14b898710834e33b944b47
Marcus Hughes <hughes.jmb@gmail.com>
2025-05-11 15:54:29 -0600

    reorder pytest so Prefect is happy

--- punchbowl/level3/tests/test_flow.py
+++ punchbowl/level3/tests/test_flow.py
@@ -11,6 +11,10 @@ from punchbowl.level3.flow import level3_core_flow
 
 THIS_DIRECTORY = pathlib.Path(__file__).parent.resolve()
 
+# We run these tests first because when they were at the end they timed out on Prefect's fixture.
+# Putting them first fixes that. Not sure why.
+
+@pytest.mark.order(1)
 def test_core_flow_runs_with_filenames(sample_ndcube, tmpdir):
     data_list = [sample_ndcube(shape=(3, 10, 10), code="PTM", level="2") for _ in range(12)]
     before_f_corona_model_path = os.path.join(tmpdir, "before_f_corona.fits")
@@ -34,6 +38,7 @@ def test_core_flow_runs_with_filenames(sample_ndcube, tmpdir):
                               None)
     assert isinstance(output[0], NDCube)
 
+@pytest.mark.order(1)
 def test_core_flow_clear_runs_with_filenames(sample_ndcube, tmpdir):
     data_list = [sample_ndcube(shape=(10, 10), code="CTM", level="2") for _ in range(12)]
     before_f_corona_model_path = os.path.join(tmpdir, "before_f_corona.fits")
@@ -57,6 +62,7 @@ def test_core_flow_clear_runs_with_filenames(sample_ndcube, tmpdir):
                               None)
     assert isinstance(output[0], NDCube)
 
+@pytest.mark.order(1)
 @pytest.mark.parametrize("num_files", [1, 3])
 def test_core_flow_runs_with_objects_and_calibration_files(tmpdir, sample_ndcube, num_files):
     data_list = [sample_ndcube(shape=(3, 10, 10), code="PTM", level="2") for _ in range(num_files)]
