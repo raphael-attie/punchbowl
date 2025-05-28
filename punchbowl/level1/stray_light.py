04dfb30dea60b083228c8d26a28b4dd941d72d41
Sam Van Kooten <vankooten.sam@gmail.com>
2025-05-28 14:26:20 -0600

    Fix for when no stray light model is provided

--- punchbowl/level1/stray_light.py
+++ punchbowl/level1/stray_light.py
@@ -59,36 +59,37 @@ def remove_stray_light_task(data_object: NDCube, stray_light_path: pathlib.Path 
         modified version of the input with the stray light removed
 
     """
-    stray_light_path = pathlib.Path(stray_light_path)
     if stray_light_path is None:
         data_object.meta.history.add_now("LEVEL1-remove_stray_light", "Stray light correction skipped")
-    elif not stray_light_path.exists():
+        return data_object
+
+    stray_light_path = pathlib.Path(stray_light_path)
+    if not stray_light_path.exists():
         msg = f"File {stray_light_path} does not exist."
         raise InvalidDataError(msg)
+    stray_light_model = load_ndcube_from_fits(stray_light_path)
+
+    stray_light_model_date = Time(stray_light_model.meta["DATE-OBS"].value)
+    observation_date = Time(data_object.meta["DATE-OBS"].value)
+    if abs((stray_light_model_date - observation_date).to("day").value) > 14:
+        msg=f"Calibration file {stray_light_path} contains data created greater than 2 weeks from the obsveration"
+        warnings.warn(msg,LargeTimeDeltaWarning)
+
+    if stray_light_model.meta["TELESCOP"].value != data_object.meta["TELESCOP"].value:
+        msg=f"Incorrect TELESCOP value within {stray_light_path}"
+        warnings.warn(msg, IncorrectTelescopeWarning)
+    elif stray_light_model.meta["OBSLAYR1"].value != data_object.meta["OBSLAYR1"].value:
+        msg=f"Incorrect polarization state within {stray_light_path}"
+        warnings.warn(msg, IncorrectPolarizationStateWarning)
+    elif stray_light_model.data.shape != data_object.data.shape:
+        msg = f"Incorrect stray light function shape within {stray_light_path}"
+        raise InvalidDataError(msg)
     else:
-        stray_light_model = load_ndcube_from_fits(stray_light_path)
-
-        stray_light_model_date = Time(stray_light_model.meta["DATE-OBS"].value)
-        observation_date = Time(data_object.meta["DATE-OBS"].value)
-        if abs((stray_light_model_date - observation_date).to("day").value) > 14:
-            msg=f"Calibration file {stray_light_path} contains data created greater than 2 weeks from the obsveration"
-            warnings.warn(msg,LargeTimeDeltaWarning)
-
-        if stray_light_model.meta["TELESCOP"].value != data_object.meta["TELESCOP"].value:
-            msg=f"Incorrect TELESCOP value within {stray_light_path}"
-            warnings.warn(msg, IncorrectTelescopeWarning)
-        elif stray_light_model.meta["OBSLAYR1"].value != data_object.meta["OBSLAYR1"].value:
-            msg=f"Incorrect polarization state within {stray_light_path}"
-            warnings.warn(msg, IncorrectPolarizationStateWarning)
-        elif stray_light_model.data.shape != data_object.data.shape:
-            msg = f"Incorrect stray light function shape within {stray_light_path}"
-            raise InvalidDataError(msg)
-        else:
-            data_object.data[:, :] -= stray_light_model.data[:, :]
-            uncertainty = 0
-            # TODO: when we have real uncertainties, use them
-            # uncertainty = stray_light_model.uncertainty.array # noqa: ERA001
-            data_object.uncertainty.array[...] = np.sqrt(data_object.uncertainty.array**2 + uncertainty**2)
-            data_object.meta.history.add_now("LEVEL1-remove_stray_light",
-                                             f"stray light removed with {os.path.basename(str(stray_light_path))}")
+        data_object.data[:, :] -= stray_light_model.data[:, :]
+        uncertainty = 0
+        # TODO: when we have real uncertainties, use them
+        # uncertainty = stray_light_model.uncertainty.array # noqa: ERA001
+        data_object.uncertainty.array[...] = np.sqrt(data_object.uncertainty.array**2 + uncertainty**2)
+        data_object.meta.history.add_now("LEVEL1-remove_stray_light",
+                                         f"stray light removed with {os.path.basename(str(stray_light_path))}")
     return data_object
