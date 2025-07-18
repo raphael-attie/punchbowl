be50734e21b6bd5276b588300651b9168480195b
Sam Van Kooten <vankooten.sam@gmail.com>
2025-07-18 16:34:47 -0600

    Fix logging in multiprocessing

--- punchbowl/levelq/pca.py
+++ punchbowl/levelq/pca.py
@@ -1,5 +1,10 @@
 import os
+import logging
+import threading
+import contextlib
+import contextvars
 import multiprocessing as mp
+import logging.handlers
 from itertools import repeat
 
 import numpy as np
@@ -16,10 +21,10 @@ from threadpoolctl import threadpool_limits
 from punchbowl.data import NormalizedMetadata
 from punchbowl.levelq.limits import LimitSet
 from punchbowl.prefect import punch_task
-from punchbowl.util import load_image_task, DataLoader
-
+from punchbowl.util import DataLoader, load_image_task
 
 _all_files_to_fit = None
+_log_queue = None
 
 
 @punch_task
@@ -35,7 +40,8 @@ def pca_filter(input_cubes: list[NDCube], files_to_fit: list[NDCube | DataLoader
     try:
         global _all_files_to_fit # noqa: PLW0603
         _all_files_to_fit, bodies_in_quarter, to_subtract = load_files(input_cubes, files_to_fit, outlier_limits)
-        with mp.Pool(n_strides) as p:
+        # 25 threads per worker would saturate all our cores if they all run at once, but experience shows they don't.
+        with _log_forwarder(), threadpool_limits(25), mp.Pool(n_strides) as p:
             for subtracted_cube_indices, subtracted_images in p.starmap(
                     pca_filter_one_stride, zip(range(n_strides), repeat(n_strides), repeat(bodies_in_quarter),
                     repeat(to_subtract), repeat(n_components), repeat(med_filt))):
@@ -115,7 +121,11 @@ def load_files(input_cubes: list[NDCube], files_to_fit: list[NDCube | str | Data
 def pca_filter_one_stride(stride: int, n_strides: int, bodies_in_quarter: np.ndarray, to_subtract: np.ndarray,
                           n_components: int, med_filt: int) -> np.ndarray:
     """Run PCA-based filtering for one stride position."""
-    logger = get_run_logger()
+    # This sets up a logger that forwards entries through a queue to the main process, where they can be forwarded to
+    # Prefect
+    logger = logging.getLogger()
+    logger.addHandler(logging.handlers.QueueHandler(_log_queue))
+    logger.setLevel(logging.INFO)
 
     quarter_slices = [
         np.s_[0:_all_files_to_fit.shape[1] // 2, 0:_all_files_to_fit.shape[2] // 2],
@@ -125,20 +135,26 @@ def pca_filter_one_stride(stride: int, n_strides: int, bodies_in_quarter: np.nda
     ]
 
     stride_filter = np.arange(len(_all_files_to_fit)) % n_strides == stride
+    # This will mark the images we'll be subtracting from---those are the only ones we'll drop from the fitting
     to_subtract_filter = stride_filter * (to_subtract >= 0)
     if not np.any(to_subtract_filter):
         logger.info(f"Stride {stride} has no images to subtract")
         return [], []
 
     images_to_subtract = _all_files_to_fit[to_subtract_filter]
+    # This tracks where each image-to-be-subtracted is in the main list of NDCubes
     subtracted_cube_indices = to_subtract[to_subtract_filter]
 
+    # We need to PCA separately for each quarter of the image
     filtered_by_quarter = np.empty_like(images_to_subtract)
     for i, quarter_slice in enumerate(quarter_slices):
-        logger.info(f"Starting to filter quarter {i+1}")
+        tag = f"stride {stride}, quarter {i+1}"
+        logger.info(f"Starting to filter {tag}")
+        # We mark the images that don't have any planets in the quarter we're filtering for (since those can
+        # contaminate the PCA components)
         no_bodies_in_quarter = np.all(bodies_in_quarter[:, :, i] == False, axis=1) # noqa: E712
         images_to_fit = _all_files_to_fit[no_bodies_in_quarter * ~to_subtract_filter]
-        filtered = run_pca_filtering(images_to_subtract, images_to_fit, n_components, med_filt)
+        filtered = run_pca_filtering(images_to_subtract, images_to_fit, n_components, med_filt, tag, logger)
 
         filtered_by_quarter[:, *quarter_slice] = filtered[:, *quarter_slice]
 
@@ -146,25 +162,23 @@ def pca_filter_one_stride(stride: int, n_strides: int, bodies_in_quarter: np.nda
 
 
 def run_pca_filtering(images_to_subtract: np.ndarray, images_to_fit: np.ndarray, n_components: int,
-                      med_filt: int) -> np.ndarray:
+                      med_filt: int, tag: str, logger: logging.Logger) -> np.ndarray:
     """Run PCA filtering."""
-    logger = get_run_logger()
-    with threadpool_limits(10):
-        pca = PCA(n_components=n_components)
-        pca.fit(images_to_fit.reshape((len(images_to_fit), -1)))
-        logger.info("Fitting finished")
+    pca = PCA(n_components=n_components)
+    pca.fit(images_to_fit.reshape((len(images_to_fit), -1)))
+    logger.info(f"Fitting finished for {tag}")
 
-        transformed = pca.transform(images_to_subtract.reshape((len(images_to_subtract), -1)))
+    transformed = pca.transform(images_to_subtract.reshape((len(images_to_subtract), -1)))
 
-        if med_filt:
-            for i in range(len(pca.components_)):
-                comp = pca.components_[i].reshape(images_to_fit.shape[1:])
-                comp = scipy.signal.medfilt2d(comp, med_filt)
-                pca.components_[i] = comp.ravel()
-            logger.info("Median smoothing finished")
+    if med_filt:
+        for i in range(len(pca.components_)):
+            comp = pca.components_[i].reshape(images_to_fit.shape[1:])
+            comp = scipy.signal.medfilt2d(comp, med_filt)
+            pca.components_[i] = comp.ravel()
+        logger.info(f"Median smoothing finished for {tag}")
 
-        reconstructed = pca.inverse_transform(transformed).reshape(images_to_subtract.shape)
-        return images_to_subtract - reconstructed
+    reconstructed = pca.inverse_transform(transformed).reshape(images_to_subtract.shape)
+    return images_to_subtract - reconstructed
 
 
 def find_bodies_in_image(frame: str | NDCube | WCS) -> list:
@@ -198,3 +212,48 @@ def find_bodies_in_image(frame: str | NDCube | WCS) -> list:
             in_right and in_top]
         results.append(body_in_quarter)
     return results
+
+
+@contextlib.contextmanager
+def _log_forwarder() -> None:
+    # This logging situation is kind of a mess. We really want to parallelize with multiprocessing so we can fork and
+    # not copy all the images to each worker. But Prefect logging from those forked processes doesn't work (nor does
+    # logging print statements), so in the workers we need to set up a logger than puts log entries in a Queue,
+    # and then on the main process side we need to spawn a thread that monitors the queue and forwards log entries to
+    # Prefect.
+
+    # The log queue has to be a global variable so the worker processes can retrieve it
+    global _log_queue # noqa: PLW0603
+
+    def _logger_consumer_thread(logging_q: mp.Queue) -> None:
+        # This gets the right Prefect metadata attached to our forwarded log entries
+        current_thread_context = dict(contextvars.copy_context().items())
+
+        logger = get_run_logger()
+
+        while True:
+            record = logging_q.get()
+            if record is None:
+                break
+            logger.log(level=record.levelno, msg=record.msg, extra=current_thread_context)
+
+    try:
+        _log_queue = mp.Queue()
+        # This is part of getting the right Prefect metadata attached to our forwarded log entries
+        current_thread_context = contextvars.copy_context()
+
+        logging_q_consumer_thread = threading.Thread(
+            target=current_thread_context.run,
+            args=(lambda: _logger_consumer_thread(_log_queue),),
+            # Daemonizing the thread allows the Python process to quit when all non-daemonized threads terminate
+            daemon=True,
+        )
+        logging_q_consumer_thread.start()
+        yield
+    finally:
+        try: # noqa: SIM105
+            # Signal to the thread it can quit
+            _log_queue.put(None)
+        except: # noqa: S110, E722
+            pass
+        _log_queue = None
