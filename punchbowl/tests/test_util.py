f3828c9c91026fa002d6c0ec92398ded57a702ce
Chris Lowder <chris.lowder@swri.org>
2025-07-22 10:55:12 -0600

    adds and tests data interpolation function

--- punchbowl/tests/test_util.py
+++ punchbowl/tests/test_util.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timezone
+
 import numpy as np
 import pytest
 from astropy.wcs import WCS
 from ndcube import NDCube
 
-from punchbowl.util import find_first_existing_file
+from punchbowl.data.tests.test_punch_io import sample_ndcube
+from punchbowl.util import find_first_existing_file, interpolate_data
 
 
 def test_find_first_existing_file():
@@ -14,3 +17,19 @@ def test_find_first_existing_file():
 def test_find_first_existing_file_raises_error_on_all_none():
     with pytest.raises(RuntimeError):
         first_cube = find_first_existing_file([None, None, None])
+
+def test_interpolate_data(sample_ndcube):
+    cube_before = sample_ndcube((10,10))
+    cube_before.data[:] = 1
+    cube_before.meta['DATE-OBS'] = str(datetime(2024, 1, 1, 0, 0, 0))
+
+    cube_after = sample_ndcube((10,10))
+    cube_after.data[:] = 2
+    cube_after.meta['DATE-OBS'] = str(datetime(2024, 1, 2, 0, 0, 0))
+
+    reference_time = datetime(2024, 1, 1, 12, 0, 0, tzinfo=timezone.utc)
+
+    data_interpolated = interpolate_data(cube_before, cube_after, reference_time)
+
+    assert isinstance(data_interpolated, np.ndarray)
+    assert np.all(data_interpolated == 1.5)
