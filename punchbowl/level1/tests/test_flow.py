1b7d215c75a05121b7d7194aae026d7790aaf975
Sam Van Kooten <vankooten.sam@gmail.com>
2025-08-05 15:49:34 -0600

    Improve test coverage

--- punchbowl/level1/tests/test_flow.py
+++ punchbowl/level1/tests/test_flow.py
@@ -14,7 +14,7 @@ THIS_DIRECTORY = pathlib.Path(__file__).parent.resolve()
 def test_early_core_flow_runs_with_filenames(sample_ndcube, tmpdir, return_x_file):
         input_name = os.path.join(tmpdir, "test_input.fits")
         output_name = os.path.join(tmpdir, "test_output.fits")
-        sample_data = sample_ndcube(shape=(10, 10), code="CR1", level="1")
+        sample_data = sample_ndcube(shape=(10, 10), code="PM1", level="1")
         sample_data.meta["RAWBITS"] = 16
         sample_data.meta["COMPBITS"] = 10
         sample_data.meta["GAINBTM"] = 4.9
@@ -25,9 +25,13 @@ def test_early_core_flow_runs_with_filenames(sample_ndcube, tmpdir, return_x_fil
 
         quartic_coefficient_path = THIS_DIRECTORY / "data" / "test_quartic_coeffs.fits"
         vignetting_path = THIS_DIRECTORY / "data" / "PUNCH_L1_GR1_20240222163425_v1.fits"
+        stray_light_before = THIS_DIRECTORY / "data" / "PUNCH_L1_SM1_20240222163425_v1.fits"
+        stray_light_after = THIS_DIRECTORY / "data" / "PUNCH_L1_SM1_20240222163425_v1.fits"
         output = level1_early_core_flow([input_name],
                                   quartic_coefficient_path=quartic_coefficient_path,
                                   vignetting_function_path=vignetting_path,
+                                  stray_light_before_path=stray_light_before,
+                                  stray_light_after_path=stray_light_after,
                                   output_filename=[output_name], do_align=False,
                                   return_with_stray_light=return_x_file)
         assert isinstance(output[0], NDCube)
