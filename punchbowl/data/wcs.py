bca31934d3bd29ad31e3f73dece8664d4e3603ce
Marcus Hughes <hughes.jmb@gmail.com>
2025-08-06 10:29:37 -0600

    remove extra noqa

--- punchbowl/data/wcs.py
+++ punchbowl/data/wcs.py
@@ -163,7 +163,7 @@ def get_p_angle(time: str="now") -> u.deg:
 def hpc_to_gcrs(HPcoord, GCRSframe):  # noqa: ANN201, N803, ANN001
     """Convert helioprojective to GCRS."""
     if not _times_are_equal(HPcoord.obstime, GCRSframe.obstime):
-        raise ValueError("Obstimes are not equal")  # noqa: EM101
+        raise ValueError("Obstimes are not equal")
     obstime = HPcoord.obstime or GCRSframe.obstime
 
     # Compute the three angles we need
@@ -221,7 +221,7 @@ def hpc_to_gcrs(HPcoord, GCRSframe):  # noqa: ANN201, N803, ANN001
 def gcrs_to_hpc(GCRScoord, Helioprojective): # noqa: ANN201, N803, ANN001
     """Convert GCRS to HPC."""
     if not _times_are_equal(GCRScoord.obstime, Helioprojective.obstime):
-        raise ValueError("Obstimes are not equal")  # noqa: EM101
+        raise ValueError("Obstimes are not equal")
     obstime = GCRScoord.obstime or Helioprojective.obstime
 
     # Compute the three angles we need
